e09662f3f6b109aae20ff02009c5e9db1a913459
Ryan Crimmins <Ryan.Crimmins@affiliate.oliverwyman.com>
2015-11-14 19:51:31 +0000

    mastercard data smaple

---
+++ data/mastercardLoyaltyOffers.js
@@ -0,0 +1,108 @@
+/*
+
+{
+    "Response": {
+    "Status": {
+        "Code": "600",
+            "Message": "Success"
+    },
+    "TotalCount": "3",
+        "CurrentPage": "1",
+        "NumberOfPages": "1",
+        "ItemsPerPage": "3",
+        "Items": [
+        {
+            "CurrencyCode": "GBP",
+            "Headline": "Shop at TMLewin and get 20% back on purchase of £50 or more",
+            "Language": "English",
+            "Merchant": {
+                "Category": "DEPARTMENTSTORE",
+                "MerchantDistance": "0.3",
+                "Name": "TM Lewin",
+                "StoreLocatorUrl": "http://www.example.com/locations/",
+                "WebsiteUrl": "www.tmlewin.com",
+                "Addresses": {
+                    "Latitude": "51.5082207",
+                    "Longitude": "-0.2230506",
+                    "Street": "Unit 2088 -2089, Westfield White City, Westfield London, Watford W12 7GD"
+                }
+            },
+            "MerchantImageURL": "http://demandware.edgesuite.net/sits_pod23/dw/image/v2/AAEX_PRD/on/demandware.static/-/Sites-tmlgbp-Library/default/dw9ea0af03/images/logo.png?sfrm=png",
+            "OfferId": "c7dcfca7-cf35-36b0-9e67-d4f363d643e0",
+            "OfferSource": "Personalized Loyalty Offers",
+            "OfferType": "POSTPAIDCREDIT",
+            "ProductName": "merchandise",
+            "RedemptionMode": "CASH",
+            "ShortDescription": "T.M.Lewin, expert Jermyn Street shirtmaker since 1898, for hand-finished men's shirts, wool suits, 100% silk ties and women's work wear.",
+            "EventEndDate": "2015-12-01",
+            "EventStartDate": "2015-02-28",
+            "ActiveSc": "AVAILABLE",
+            "Discount": "20.0",
+            "DiscountType": "PERCENTAGE",
+            "Price": "0.0"
+        },
+        {
+            "CurrencyCode": "GBP",
+            "Headline": "Get 30% off your meal at Jamie's Italian",
+            "Language": "English",
+            "Merchant": {
+                "Category": "RESTAURANT",
+                "MerchantDistance": "0.2",
+                "Name": "Jamie's Italian",
+                "StoreLocatorUrl": "http://www.example.com/locations/",
+                "WebsiteUrl": "www.jamiesitalian.com",
+                "Addresses": {
+                    "Latitude": "51.5076865",
+                    "Longitude": "-0.2239196",
+                    "Street": "1078, Westfield Shopping Centre, Ariel Way, Shepherd's Bush, London W12 7GB"
+                }
+            },
+            "MerchantImageURL": "https://www.jamieoliver.com/italian/images/shared/logo.png",
+            "OfferId": "c7dcfca7-cf35-36b0-9e67-d4f363d643e1",
+            "OfferSource": "Personalized Loyalty Offers",
+            "OfferType": "POSTPAIDCREDIT",
+            "ProductName": "merchandise",
+            "RedemptionMode": "CASH",
+            "ShortDescription": "Celebrity chef's relaxed Italian chain, with rustic menu and antipasti bar.",
+            "EventEndDate": "2015-12-01",
+            "EventStartDate": "2015-02-28",
+            "ActiveSc": "AVAILABLE",
+            "Discount": "20.0",
+            "DiscountType": "PERCENTAGE",
+            "Price": "0.0"
+        },
+        {
+            "CurrencyCode": "GBP",
+            "Headline": "2 for 1 tickets on Sunday afternoon",
+            "Language": "English",
+            "Merchant": {
+                "Category": "CINEMA",
+                "MerchantDistance": "0.3",
+                "Name": "VUE Cinema",
+                "StoreLocatorUrl": "http://www.example.com/locations/",
+                "WebsiteUrl": "www.myvue.com",
+                "Addresses": {
+                    "Latitude": "51.5043503",
+                    "Longitude": "-0.2517393",
+                    "Street": "\n4006, Ariel Way, White City, London W12 7GF"
+                }
+            },
+            "MerchantImageURL": "http://www.myvue.com/images/vue_logo_header.png",
+            "OfferId": "c7dcfca7-cf35-36b0-9e67-d4f363d643e2",
+            "OfferSource": "Personalized Loyalty Offers",
+            "OfferType": "POSTPAIDCREDIT",
+            "ProductName": "merchandise",
+            "RedemptionMode": "CASH",
+            "ShortDescription": "Vue Westfield London is the UK's biggest and most technologically advanced, fully digital, 17 screen multiplex cinema.",
+            "EventEndDate": "2015-12-01",
+            "EventStartDate": "2015-02-28",
+            "ActiveSc": "AVAILABLE",
+            "Discount": "20.0",
+            "DiscountType": "PERCENTAGE",
+            "Price": "0.0"
+        }
+    ]
+}
+}
+
+    */
