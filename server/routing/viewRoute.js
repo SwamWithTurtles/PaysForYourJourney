22aaf125eeee91dc49b59cbe6ade1940d729f99e
David Simons <12e.david@gmail.com>
2015-11-14 16:04:02 +0000

    Take user to parametrised page

--- server/routing/viewRoute.js
+++ server/routing/viewRoute.js
@@ -2,6 +2,10 @@ var setUp = function(app) {
     app.get('/', function(request, response, next) {
         response.render('index');
     });
+
+    app.get('/destination', function(request, response, next) {
+        response.render('destination');
+    });
 };
 
 module.exports.setUp = setUp;
