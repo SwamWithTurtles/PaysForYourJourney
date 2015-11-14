1ac76f463a7482636a878c0e2ac60b622abab3dc
David Simons <12e.david@gmail.com>
2015-11-14 23:48:04 +0000

    Add offers to journey API

--- server/bl/mastercardDataParser.js
+++ server/bl/mastercardDataParser.js
@@ -3,6 +3,12 @@ var _ = require('lodash');
 
 module.exports.populateTflData = function(journeys, callback) {
 
+    _.forEach(journeys, function(j) {
+        _.forEach(j.steps, function(step) {
+            step.offers = ["10% off at TM Lewin"]
+        })
+    })
+
     mastercardGateway.getData(function(opportunities) {
         callback(journeys);
     })
